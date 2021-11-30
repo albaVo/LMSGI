da7eb0d21080874562d2562507dd2eab04acabcf
albaVo <alba12vo@gmail.com>
2021-11-30 17:42:37 +0100

    Introducció JS

--- HTML/3-RWD/2021/maq2-MFirst/js/app.js
+++ HTML/3-RWD/2021/maq2-MFirst/js/app.js
@@ -1,24 +1,22 @@
-
 console.log ('Cargando app.js');
 const iva = 0.16;
 
 let pcoste = 2000;
 
-
 // la función anterior se reescribe en ntoación Arrow Function
 const calcularPVP = (precio, impuesto) => {
-    /*console.log('Precio de Coste = ', precio, '€');
+    console.log('Precio de Coste = ', precio, '€');
     console.log('iva = ', iva * 100, '%', '-->', precio * impuesto);
-    console.log('PVP = ', precio * (1 + impuesto), '€');*/
+    console.log('PVP = ', precio * (1 + impuesto), '€');
     return precio * (1 + impuesto);
 }
 
 //llamadas a la función 
-/*let ptotal = calcularPVP(20, iva);*/
+//let ptotal = calcularPVP(20, iva);
 
 function sumar1 (x,y){
     // instrucción 1;
-    // instrufcción 2;
+    // instrucción 2;
     // ...
     // instrución n;
     return (x + y)
@@ -44,10 +42,9 @@ console.log( cubo(5) );
 
 const potencia = (b, e) => {
     let res = 1;
-    for(i = 1; i <= e, i++){
+    for(i = 1; i <= e; i++){
         res = res * b
     }
     return res;
 }
-
 console.log(potencia (2,4));
