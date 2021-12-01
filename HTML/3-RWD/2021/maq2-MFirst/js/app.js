aa23992aa471c0fbfebd5aa93a6a8ed8b64084c0
albaVo <alba12vo@gmail.com>
2021-12-01 14:28:10 +0100

    Carga Dinamica

--- HTML/3-RWD/2021/maq2-MFirst/js/app.js
+++ HTML/3-RWD/2021/maq2-MFirst/js/app.js
@@ -1,68 +1,40 @@
-console.log ('Cargando app.js');
-const iva = 0.16;
 
-let pcoste = 2000;
 
-// la función anterior se reescribe en ntoación Arrow Function
-const calcularPVP = (precio, impuesto) => {
-    console.log('Precio de Coste = ', precio, '€');
-    console.log('iva = ', iva * 100, '%', '-->', precio * impuesto);
-    console.log('PVP = ', precio * (1 + impuesto), '€');
-    return precio * (1 + impuesto);
+// carga dinámica de páginas 
+function inicio() {
+    console.log('documento app cargado');
+    console.log(document);
+    const vmain = document.querySelector('main section');
+    //vmain.innerHTML = '<h1>Hola Caracola</h1>'
+    
+    //detectar clicks de ratón en el header nav
+    const servicios = document.querySelector('header nav a#servicios');
+    const formacion = document.querySelector('header nav a#formacion');
+    const trabajos = document.querySelector('header nav a#trabajos');
+
+    servicios.addEventListener("click", () => {
+        console.log('has pinchado en servicios');
+        //leer los ficheros html
+        fetch('html/servicios.html')
+            .then(respuesta =>  {
+                return respuesta.text()
+            })
+            .then(respuestaHtml => {
+                console.log(respuestaHtml)
+                vmain.innerHTML = respuestaHtml;
+            }
+        )
+    });
+
+    formacion.addEventListener("click", () => {
+        console.log('has pinchado en formacion');
+        fetch('html/formacion.html')
+            .then(respuesta =>  {
+                return respuesta.text()
+            })
+            .then(respuestaHtml => {
+                console.log(respuestaHtml)
+            }
+        )
+    });
 }
-
-//llamadas a la función 
-//let ptotal = calcularPVP(20, iva);
-
-function sumar1 (x,y){
-    // instrucción 1;
-    // instrucción 2;
-    // ...
-    // instrución n;
-    return (x + y)
-}
-
-/* solo cuando el cuerpo de la función tiene 1 sola instrucción
-const sumar2 = (x, y) => {
-    return x + y;
-}
-*/
-const sumar2 = (x,y) => x + y;
-
-let suma = sumar2(13,6);
-console.log(suma);
-
-const cubo1 = ( x ) => {
-    return (x * x * x)
-} 
-
-const cubo = (x) => x*x*x;
-console.log( cubo(5) );
-
-
-const potencia = (b, e) => {
-    let res = 1;
-    for(i = 1; i <= e; i++){
-        res = res * b
-    }
-    return res;
-}
-//console.log(potencia (2,4));
-
-// sumar los 10 primeros números
-const suma10 = ( n ) => {
-    let stotal = 0;
-    for(i = 0; i <= n; i++){
-        stotal = stotal + i;
-    }
-    return stotal;
-}
-console.log(suma10( 2 ));
-console.log(suma10(5));
-console.log(suma10(100));
-
-// calcular el factorial de un número
-const factorial ( n ) => {
-
-}
-console.log (factorial (5));
---
+++ HTML/3-RWD/2021/maq2-MFirst/js/documentacion/app1.js
@@ -0,0 +1,69 @@
+console.log ('Cargando app.js');
+const iva = 0.16;
+
+let pcoste = 2000;
+
+// la función anterior se reescribe en ntoación Arrow Function
+const calcularPVP = (precio, impuesto) => {
+    console.log('Precio de Coste = ', precio, '€');
+    console.log('iva = ', iva * 100, '%', '-->', precio * impuesto);
+    console.log('PVP = ', precio * (1 + impuesto), '€');
+    return precio * (1 + impuesto);
+}
+
+//llamadas a la función 
+//let ptotal = calcularPVP(20, iva);
+
+function sumar1 (x,y){
+    // instrucción 1;
+    // instrucción 2;
+    // ...
+    // instrución n;
+    return (x + y)
+}
+
+/* solo cuando el cuerpo de la función tiene 1 sola instrucción
+const sumar2 = (x, y) => {
+    return x + y;
+}
+*/
+const sumar2 = (x,y) => x + y;
+
+let suma = sumar2(13,6);
+console.log(suma);
+
+const cubo1 = ( x ) => {
+    return (x * x * x)
+} 
+
+const cubo = (x) => x*x*x;
+console.log( cubo(5) );
+
+
+const potencia = (b, e) => {
+    let res = 1;
+    for(i = 1; i <= e; i++){
+        res = res * b
+    }
+    return res;
+}
+//console.log(potencia (2,4));
+
+// sumar los 10 primeros números
+const suma10 = ( n ) => {
+    let stotal = 0;
+    for(i = 0; i <= n; i++){
+        stotal = stotal + i;
+    }
+    return stotal;
+}
+console.log(suma10( 2 ));
+console.log(suma10(5));
+console.log(suma10(100));
+
+// calcular el factorial de un número
+const factorial = ( n ) => {
+    let factor = 0;
+    
+}
+console.log (factorial (5));
